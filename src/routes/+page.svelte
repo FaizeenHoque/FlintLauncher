15b141539dc54ca4a9f20878820e168bec8bd63e
Soorman <sirtv490@gmail.com>
2026-03-29 00:34:19 +0600

    feat: update navigation to home and add new statistics page with launch button

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<main>
+    <div class="flex flex-row gap-7 text-xl p-4 font-roboto font-medium">
+
+        <div>
+           <h1 class="text-gray-400">Storage Used:</h1> 
+           <h2 class="text-white">0 GB</h2>
+        </div>
+
+        <div>
+            <h1 class="text-gray-400">Play Time:</h1>
+            <h2 class="text-white">0 Hours</h2>
+        </div>
+
+        <div>
+            <h1 class="text-gray-400">Last Played:</h1>
+            <h2 class="text-white">0 Hours ago</h2>
+        </div>
+
+        <div>
+           <h1 class="text-gray-400">Account Selected:</h1> 
+           <h2 class="text-white">None</h2>
+        </div>
+
+        <div>
+           <h1 class="text-gray-400">Version Selected:</h1> 
+           <select class="w-full bg-neutral-800 text-white font-rubik outline-none ring-0 focus:ring-0 focus:outline-none border-0">
+             <option>Hi</option>
+           </select>
+        </div>
+
+    </div>
+
+    <div>
+        <button class="text-white text-xl font-roboto font-medium py-5 px-15 m-3 bg-green-400 rounded-2xl transition-all ease-in duration-300 hover:bg-green-500 hover:shadow-green-900 shadow-lg active:bg-green-900">Launch</button>
+    </div>
+</main>
