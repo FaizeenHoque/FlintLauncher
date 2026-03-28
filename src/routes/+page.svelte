0772dbc5769fc8c5d7d59c81de4b3bb746f98e96
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-03-28 20:54:43 +0600

    feat: add account creation functionality and integrate with UI

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { createAccount } from "../lib/account";
+
+    let username = $state("");
+
+    async function handleConfirm() {
+        try {
+            await createAccount(username);
+            console.log("done");
+        } catch (e) {
+            console.error("failed:", e);
+        }
+    }
+</script>
+
+<main>
+    
+</main>``
