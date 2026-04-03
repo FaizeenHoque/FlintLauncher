737c008afdfcb9459c6105a7bcc630364cf2512f
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-04-03 20:58:11 +0600

    Made background for landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="relative h-screen w-screen overflow-hidden">
+	<!-- Floaters (behind) -->
+	<div class="absolute inset-0">
+		<div
+			class="relative right-[10vw] -bottom-[20vh] flow-root h-[20vh] w-[20vw] -rotate-20 bg-[#1C1B1B] opacity-50"
+		></div>
+		<div
+			class="relative -bottom-[50vh] left-[80vw] h-[40vh] w-[40vw] rotate-10 bg-[#1C1B1B] opacity-50"
+		></div>
+	</div>
+	<!-- Grid (on top) -->
+	<div class="relative z-10 grid h-screen w-screen place-items-center">
+		<div class="h-[80vh] w-[80vw] rounded-[12px] bg-[#82DB7E] opacity-5 blur-[60px]"></div>
+	</div>
+</main>
