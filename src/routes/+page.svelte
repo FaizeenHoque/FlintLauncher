6b50c02b9366f5b2ed0755b4a928d68f33f05823
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-04-03 22:05:19 +0600

    Finish site design with working download button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <main class="relative h-screen w-screen overflow-hidden">
 	<!-- Floaters (behind) -->
-	<div class="absolute inset-0">
+	<div class="pointer-events-none absolute inset-0">
 		<div
 			class="relative right-[10vw] -bottom-[20vh] flow-root h-[20vh] w-[20vw] -rotate-20 bg-[#1C1B1B] opacity-50"
 		></div>
@@ -10,6 +10,29 @@
 	</div>
 	<!-- Grid (on top) -->
 	<div class="relative z-10 grid h-screen w-screen place-items-center">
-		<div class="h-[80vh] w-[80vw] rounded-[12px] bg-[#82DB7E] opacity-5 blur-[60px]"></div>
+		<!-- background gradient -->
+		<div
+			class="pointer-events-none absolute h-[80vh] w-[80vw] rounded-[12px] bg-[#82DB7E] opacity-5 blur-[60px]"
+		></div>
+		<!-- CTA AREA -->
+		<div class="grid place-items-center">
+			<p
+				class="space-grotesk rounded-[11px] bg-[#82DB7E]/20 p-[2vw] text-[3vw] text-[#82DB7E] outline-[1px] outline-[#82DB7E] md:p-[0.6vw] md:text-[1vw]"
+			>
+				VERSION 1.4.0 STABLE
+			</p>
+			<h1 class="space-grotesk text-[20vw] font-bold text-white md:text-[10vw]">FLINT</h1>
+			<p class="manrope text-[4vw] font-thin text-[#E5E2E1] md:text-[1.6vw]">
+				The Modern Minecraft Launcher
+			</p>
+			<a
+				href="https://github.com/FaizeenHoque/FlintLauncher/releases/latest/download/Flint.Launcher_0.1.0_x64-setup.exe"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="space-grotesk my-[5vh] cursor-pointer bg-linear-30 from-[#82DB7E] from-0% to-[#4DA24E] to-100% px-[3vw] py-[1vh] text-[2vw] font-semibold md:my-[4vh]"
+			>
+				DOWNLOAD FOR WINDOWS 10/11
+			</a>
+		</div>
 	</div>
 </main>
